0f872856e0dd73364ac22b7608a7c144a19f442f
Nicolas <nicolas.gelin@edu.univ-fcomte.fr>
2021-06-03 21:10:10 +0200

    ajout des éléments 3 + responsive

---
+++ site_final/js/menu.js
@@ -0,0 +1,15 @@
+/*(function () {
+    "use strict"
+    var icone = document.getElementById("icone");
+    console.log(icone)
+
+    function ouvrirmenu(){
+        this.classList.toggle(".menuClose")
+    }
+
+    for (let x = 0; x<icone.length; x++) {
+        let click = icone[x];
+        click.addEventListener("click", ouvrirmenu)
+    }
+}())
+*/
